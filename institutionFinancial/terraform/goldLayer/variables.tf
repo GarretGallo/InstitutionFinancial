9d141ae0e5798dbd024f8fcb3797bde1771729fe
Garret Gallo <garretgallo06@gmail.com>
2025-08-28 17:37:59 -0400

    Delete institutionFinancial directory

--- institutionFinancial/terraform/goldLayer/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "iam_name_prefix" {
-  description = "Prefix used for all created IAM roles and policies"
-  type        = string
-  nullable    = false
-  default     = "demo-kinesis-firehose-"
-}
